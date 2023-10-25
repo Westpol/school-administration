923955728622ff6115cddd871029150a6b048b80
Westpol <81779508+Westpol@users.noreply.github.com>
2023-10-25 09:29:30 +0200

    Added veery basic structure

---
+++ main.py
@@ -0,0 +1,14 @@
+import pygame
+import time
+import json
+
+
+class Main:
+    def __init__(self):
+        pygame.init()
+        screen = pygame.display.set_mode((1000, 1000))
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main = Main()
